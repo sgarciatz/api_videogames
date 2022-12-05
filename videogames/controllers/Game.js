b8a93647d72f5b2e7cfdde068892a4aed5a12552
sgarciatz <sgarciatz@alumnos.unex.es>
2022-12-05 20:52:11 +0100

    http codes updated

--- videogames/controllers/Game.js
+++ videogames/controllers/Game.js
@@ -6,69 +6,69 @@ var Game = require('../service/GameService');
 module.exports.buy_gameGameIdPOST = function buy_gameGameIdPOST (req, res, next, gameId, userId) {
   Game.buy_gameGameIdPOST(gameId, userId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 201);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 404);
     });
 };
 
 module.exports.gamesGET = function gamesGET (req, res, next) {
   Game.gamesGET()
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 500);
     });
 };
 
 module.exports.gamesGameIdDELETE = function gamesGameIdDELETE (req, res, next, gameId) {
   Game.gamesGameIdDELETE(gameId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 404);
     });
 };
 
 module.exports.gamesGameIdGET = function gamesGameIdGET (req, res, next, gameId) {
   Game.gamesGameIdGET(gameId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 404);
     });
 };
 
 module.exports.gamesGameIdPATCH = function gamesGameIdPATCH (req, res, next, body, gameId) {
   Game.gamesGameIdPATCH(body, gameId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 404);
     });
 };
 
 module.exports.gamesPOST = function gamesPOST (req, res, next, body) {
   Game.gamesPOST(body)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 201);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 400);
     });
 };
 
 module.exports.test_qualityGameIdGET = function test_qualityGameIdGET (req, res, next, gameId, hardwareTier) {
   Game.test_qualityGameIdGET(gameId, hardwareTier)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 404);
     });
 };
--- videogames/controllers/Gameplay.js
+++ videogames/controllers/Gameplay.js
@@ -6,19 +6,19 @@ var Gameplay = require('../service/GameplayService');
 module.exports.gameplayGameIdGET = function gameplayGameIdGET (req, res, next, gameId, userId) {
   Gameplay.gameplayGameIdGET(gameId, userId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 404);
     });
 };
 
 module.exports.gameplayGameIdPOST = function gameplayGameIdPOST (req, res, next, body, userId, gameId) {
   Gameplay.gameplayGameIdPOST(body, userId, gameId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 404);
     });
 };
