5419dbb0c66a38f1906b3190792822d50507c98f
sgarciatz <sgarciatz@alumnos.unex.es>
2022-12-05 21:34:56 +0100

    APIs done.

--- videogames/service/GameService.js
+++ videogames/service/GameService.js
@@ -1,5 +1,78 @@
 'use strict';
 
+const users = [ {
+  "firstName" : "Juan",
+  "lastName" : "Díaz",
+  "password" : "password1234",
+  "servers" : [ 100, 200 ],
+  "phone" : "123456789",
+  "userId" : 100,
+  "subscriptionTier" : "HIGH-END Hardware",
+  "email" : "juanito.diaz@email.com",
+  "friends" : [ 200, 300 ],
+  "username" : "Juanito_2000",
+  "videogames" : [ 100, 200 ]
+}, {
+  "firstName" : "Paco",
+  "lastName" : "Hernández",
+  "password" : "password1234",
+  "servers" : [ 300 ],
+  "phone" : "657098334",
+  "userId" : 200,
+  "subscriptionTier" : "MID-END Hardware",
+  "email" : "paco.hernandez@email.com",
+  "friends" : [ 100 ],
+  "username" : "pakito_123",
+  "videogames" : [ 100 ]
+}, {
+  "firstName" : "Daniel",
+  "lastName" : "Cambero",
+  "password" : "password1234",
+  "servers" : [ ],
+  "phone" : "452670534",
+  "userId" : 300,
+  "subscriptionTier" : "MID-END Hardware",
+  "email" : "daniel.cambero@email.com",
+  "friends" : [ 100 ],
+  "username" : "dani_crk",
+  "videogames" : [ 100 ]
+} ];
+
+const videogames = [ 
+  {
+    "gameId" : 100,
+    "videogame-name" : "God Of War",
+    "genre" : "Fantasia",
+    "files" : "images,configuration files, design files"
+  },
+  {
+    "gameId" : 200,
+    "videogame-name" : "Stardew Valley",
+    "genre" : "RPG | Farming",
+    "files" : "images,configuration files, design files"
+  }
+];
+
+const videogameTests = [
+  {
+    "gameId" : 100,
+    "hardwareTier" : "HIGH-END hardware",
+    "cpuUtilization" : "75%",
+    "datetime" : "2022-12-31 23:59:48",
+    "latencia" : "100 ms",
+    "frecuency" : "120 fps",
+    "gpuUtilization" : "92%"
+  },
+  {
+    "gameId" : 200,
+    "hardwareTier" : "LOW-END hardware",
+    "cpuUtilization" : "58%",
+    "datetime" : "2022-12-31 23:59:48",
+    "latencia" : "100 ms",
+    "frecuency" : "220 fps",
+    "gpuUtilization" : "42%"
+  }
+];
 
 /**
  * Inserts a new game in the user library database.
@@ -9,14 +82,19 @@
  * userId Long ID of user to insert the game.
  * returns String
  **/
-exports.buy_gameGameIdPOST = function(gameId,userId) {
+exports.buy_gameGameIdPOST = function(userId, gameId) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "Videogame inserted successfully in the user library.";
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
+    const userExists = users.some(user => userId === user.userId);
+    const videogameExists = videogames.some(videogame => gameId === videogame.gameId);
+    if (userExists && videogameExists) {
+        examples['text/plain'] = 'Videogame inserted successfully in the user library.';
+        resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = 'Videogame and user not found.';
+      if (userExists) { examples['text/plain'] = 'Videogame not found.'; }
+      else { if (videogameExists) { examples['text/plain'] = 'User not found.'; } }
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
@@ -31,21 +109,12 @@ exports.buy_gameGameIdPOST = function(gameId,userId) {
 exports.gamesGET = function() {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = [ {
-  "gameId" : 100,
-  "videogame-name" : "God Of War",
-  "genre" : "Fantasia",
-  "files" : "images,configuration files, design files"
-}, {
-  "gameId" : 100,
-  "videogame-name" : "God Of War",
-  "genre" : "Fantasia",
-  "files" : "images,configuration files, design files"
-} ];
+    examples['application/json'] = videogames;
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = 'Something went wrong.';
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
@@ -61,11 +130,12 @@ exports.gamesGET = function() {
 exports.gamesGameIdDELETE = function(gameId) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "Videogame deleted.";
-    if (Object.keys(examples).length > 0) {
+    if (videogames.some(videogame => videogame.gameId === gameId)) {
+      examples['text/plain'] = "Videogame deleted.";
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = "Videogame not found.";
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
@@ -81,16 +151,13 @@ exports.gamesGameIdDELETE = function(gameId) {
 exports.gamesGameIdGET = function(gameId) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "gameId" : 100,
-  "videogame-name" : "God Of War",
-  "genre" : "Fantasia",
-  "files" : "images,configuration files, design files"
-};
-    if (Object.keys(examples).length > 0) {
+    const videogame = videogames.find(videogame => videogame.gameId === gameId);
+    if (videogame) {
+      examples['application/json'] = videogame;
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = "Videogame not found.";
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
@@ -107,11 +174,12 @@ exports.gamesGameIdGET = function(gameId) {
 exports.gamesGameIdPATCH = function(body,gameId) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "Videogame updated.";
-    if (Object.keys(examples).length > 0) {
+    if (videogames.some(videogame => videogame.gameId === gameId)) {
+      examples['text/plain'] = "Videogame updated.";
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = "Videogame not found.";
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
@@ -127,11 +195,12 @@ exports.gamesGameIdPATCH = function(body,gameId) {
 exports.gamesPOST = function(body) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "Videogame inserted successfully.";
-    if (Object.keys(examples).length > 0) {
+    if (!videogames.some(videogame => videogame['videogame-name'] === body['videogame-name'])) {
+      examples['text/plain'] = "Videogame inserted successfully.";
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = "This game already exists.";
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
@@ -145,22 +214,18 @@ exports.gamesPOST = function(body) {
  * hardwareTier String Specify the hardware tier on which the test will be carried out (LOW-END, MID-END or HIGH-END).
  * returns VideogameTest
  **/
-exports.test_qualityGameIdGET = function(gameId,hardwareTier) {
+exports.test_qualityGameIdGET = function(hardwareTier, gameId) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
-  "gameId" : 100,
-  "hardwareTier" : "MID-END hardware",
-  "cpuUtilization" : "75%",
-  "datetime" : "2022-12-31 23:59:48",
-  "latencia" : "100 ms",
-  "frecuency" : "120 fps",
-  "gpuUtilization" : "92%%"
-};
-    if (Object.keys(examples).length > 0) {
+
+    if (videogames.some(videogame => videogame.gameId === gameId)) {
+      const test = videogameTests.find(test => test.gameId === gameId);
+      test.hardwareTier = hardwareTier
+      examples['application/json'] = test
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = "Videogame not found.";
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
--- videogames/service/GameplayService.js
+++ videogames/service/GameplayService.js
@@ -1,5 +1,64 @@
 'use strict';
 
+const users = [ {
+  "firstName" : "Juan",
+  "lastName" : "Díaz",
+  "password" : "password1234",
+  "servers" : [ 100, 200 ],
+  "phone" : "123456789",
+  "userId" : 100,
+  "subscriptionTier" : "HIGH-END Hardware",
+  "email" : "juanito.diaz@email.com",
+  "friends" : [ 200, 300 ],
+  "username" : "Juanito_2000",
+  "videogames" : [ 100, 200 ]
+}, {
+  "firstName" : "Paco",
+  "lastName" : "Hernández",
+  "password" : "password1234",
+  "servers" : [ 300 ],
+  "phone" : "657098334",
+  "userId" : 200,
+  "subscriptionTier" : "MID-END Hardware",
+  "email" : "paco.hernandez@email.com",
+  "friends" : [ 100 ],
+  "username" : "pakito_123",
+  "videogames" : [ 100 ]
+}, {
+  "firstName" : "Daniel",
+  "lastName" : "Cambero",
+  "password" : "password1234",
+  "servers" : [ ],
+  "phone" : "452670534",
+  "userId" : 300,
+  "subscriptionTier" : "MID-END Hardware",
+  "email" : "daniel.cambero@email.com",
+  "friends" : [ 100 ],
+  "username" : "dani_crk",
+  "videogames" : [ 100 ]
+} ];
+
+const videogames = [ 
+  {
+    "gameId" : 100,
+    "videogame-name" : "God Of War",
+    "genre" : "Fantasia",
+    "files" : "images,configuration files, design files"
+  },
+  {
+    "gameId" : 200,
+    "videogame-name" : "Stardew Valley",
+    "genre" : "RPG | Farming",
+    "files" : "images,configuration files, design files"
+  }
+];
+
+
+const frameBuffer = [
+  {
+    "frames" : [ "frame1", "frame2", "frame3" ]
+  }
+];
 
 /**
  * Returns a buffer of frames containing 0, 1 or more frames.
@@ -9,14 +68,19 @@
  * userId Long Identifier of game to play.
  * returns FrameBuffer
  **/
-exports.gameplayGameIdGET = function(gameId,userId) {
+exports.gameplayGameIdGET = function(userId, gameId) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = [ "frame1", "frame2", "frame3" ];
-    if (Object.keys(examples).length > 0) {
+    const userExists = users.some(user => userId === user.userId);
+    const videogameExists = videogames.some(videogame => gameId === videogame.gameId);
+    if (userExists && videogameExists) {
+      examples['application/json'] = frameBuffer[0];
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = 'Videogame and user not found.';
+      if (userExists) { examples['text/plain'] = 'Videogame not found.'; }
+      else { if (videogameExists) { examples['text/plain'] = 'User not found.'; } }
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
@@ -31,14 +95,19 @@ exports.gameplayGameIdGET = function(gameId,userId) {
  * gameId Long Identifier of game to play.
  * returns String
  **/
-exports.gameplayGameIdPOST = function(body,userId,gameId) {
+exports.gameplayGameIdPOST = function(body, gameId, userId) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "UserInteraction recieved.";
-    if (Object.keys(examples).length > 0) {
+    const userExists = users.some(user => userId === user.userId);
+    const videogameExists = videogames.some(videogame => gameId === videogame.gameId);
+    if (userExists && videogameExists) {
+      examples['text/plain'] = 'UserInteraction recieved.';
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      resolve();
+      examples['text/plain'] = 'Videogame and user not found.';
+      if (userExists) { examples['text/plain'] = 'Videogame not found.'; }
+      else { if (videogameExists) { examples['text/plain'] = 'User not found.'; } }
+      reject(examples[Object.keys(examples)[0]]);
     }
   });
 }
